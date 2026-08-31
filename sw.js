5934b60d6ce411944ddce53f7cbfd0b2d8c22a64
a4dev01 <a4dev01@users.noreply.github.com>
2026-08-31 09:55:26 +0530

    Upgrade Quiet Notes with encrypted IndexedDB storage

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>e.waitUntil(caches.open('qn-v2').then(c=>c.addAll(['./','./index.html','./app.js','./styles.css','./manifest.webmanifest']))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
